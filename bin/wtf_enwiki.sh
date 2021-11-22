a3650f9deb34b24437665ab421d535dbfb18c667
Tony Bowden <tony@tmtm.com>
2021-11-22 12:07:25 +0200

    add wtf_wikipedia action

---
+++ bin/wtf_enwiki.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+IFS=$'\n'
+
+mkdir -p enwiki
+
+for page in $(qsv select enwiki html/current.csv | qsv search . | qsv dedup | qsv sort | qsv behead); do
+  echo $page
+  json=$(echo "$page" | xargs wtf_wikipedia)
+  pageid=$(printf '%s' "$json" | jq -r .pageID)
+  printf '%s' "$json" | jq -r . > enwiki/$pageid
+done
