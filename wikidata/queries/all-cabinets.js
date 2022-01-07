90aae66e4dbd6fd84043380521095e56a807488a
Tony Bowden <tony@tmtm.com>
2022-01-07 19:34:39 +0200

    update to latest all-cabinets query

--- wikidata/queries/all-cabinets.js
+++ wikidata/queries/all-cabinets.js
@@ -3,32 +3,36 @@ let rawmeta = fs.readFileSync('meta.json');
 let meta = JSON.parse(rawmeta);
 
 module.exports = function () {
+  let cabinet = meta.cabinet.parent ? `UNION { ?item wdt:P31 wd:${meta.cabinet.parent} }` : '';
+  let govment = meta.cabinet.government ? `UNION { ?item wdt:P31 wd:${meta.cabinet.government} }` : '';
+
   return `SELECT DISTINCT ?ordinal ?item ?itemLabel
                   ?inception ?inceptionPrecision
                   ?startTime ?startTimePrecision
                   ?abolished ?abolishedPrecision
                   ?endTime   ?endTimePrecision
-                  ?isa ?isaLabel ?parent ?parentLabel
                   ?replaces ?replacesLabel ?replacedBy ?replacedByLabel
                   ?follows ?followsLabel ?followedBy ?followedByLabel
   WHERE {
-    ?item wdt:P31/wdt:P279* wd:Q640506 ; wdt:P1001 wd:${meta.jurisdiction.id} .
+    { ?item wdt:P31/wdt:P279* wd:Q640506 ; wdt:P1001 wd:${meta.jurisdiction.id} }
+    ${cabinet}
+    ${govment}
+
     OPTIONAL { ?item p:P31/pq:P1545 ?ordinal }
     OPTIONAL { ?item p:P571 [ a wikibase:BestRank ; psv:P571 [wikibase:timeValue ?inception ; wikibase:timePrecision ?inceptionPrecision] ] }
     OPTIONAL { ?item p:P580 [ a wikibase:BestRank ; psv:P580 [wikibase:timeValue ?startTime ; wikibase:timePrecision ?startTimePrecision] ] }
     OPTIONAL { ?item p:P576 [ a wikibase:BestRank ; psv:P576 [wikibase:timeValue ?abolished ; wikibase:timePrecision ?abolishedPrecision] ] }
     OPTIONAL { ?item p:P582 [ a wikibase:BestRank ; psv:P582 [wikibase:timeValue ?endTime   ; wikibase:timePrecision ?endTimePrecision]   ] }
-    OPTIONAL { ?item wdt:P580 ?startTime     }
-    OPTIONAL { ?item wdt:P576 ?abolished     }
-    OPTIONAL { ?item wdt:P582 ?endTime       }
-    OPTIONAL { ?item wdt:P31 ?isa            }
-    OPTIONAL { ?item wdt:P279 ?parent        }
+
     OPTIONAL { ?item wdt:P1365 ?replaces     }
     OPTIONAL { ?item wdt:P1366 ?replacedBy   }
     OPTIONAL { ?item wdt:P155 ?follows       }
     OPTIONAL { ?item wdt:P156 ?followedBy    }
 
-    SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+    BIND(COALESCE(?inception, ?startTime) AS ?start)
+
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
   }
-  ORDER BY ?inception ?isa ?parent`
+  # ${new Date().toISOString()}
+  ORDER BY ?start ?item`
 }
