1e4d3ac31ce0a335aca4e8edd5a6382385e3d10e
Tony Bowden <tony@tmtm.com>
2022-04-29 14:52:28 +0300

    less verbose comparisons

--- bin/compare-official.sh
+++ bin/compare-official.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-bundle exec ruby bin/scraper/official.rb | ifne tee data/official.csv
-bundle exec ruby bin/scraper/wikidata.rb meta.json | sed -e 's/T00:00:00Z//g' | qsv dedup -s psid | ifne tee data/wikidata.csv
+bundle exec ruby bin/scraper/official.rb > data/official.csv
+bundle exec ruby bin/scraper/wikidata.rb meta.json | sed -e 's/T00:00:00Z//g' | qsv dedup -s psid > data/wikidata.csv
 bundle exec ruby bin/diff.rb | tee data/diff.csv
--- sources/reykjavik-mayor-enwiki/compare.sh
+++ sources/reykjavik-mayor-enwiki/compare.sh
@@ -2,8 +2,8 @@
 
 cd $(dirname $0)
 
-bundle exec ruby scraper.rb $(jq -r .source meta.json) | ifne tee scraped.csv
-wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' -e 's#http://www.wikidata.org/entity/##g' | qsv dedup -s psid | ifne tee wikidata.csv
+bundle exec ruby scraper.rb $(jq -r .source meta.json) > scraped.csv
+wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' -e 's#http://www.wikidata.org/entity/##g' | qsv dedup -s psid > wikidata.csv
 bundle exec ruby diff.rb | qsv sort -s startdate | qsv select 1,3,2,4,5 | tee diff.csv
 
 cd ~-
